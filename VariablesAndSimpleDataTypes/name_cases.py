ef0b33c5a311d45daf57ccd187b8fc2f62d1aee3
oktamorph <oktamorph@gmail.com>
2023-10-29 21:31:34 +0100

    add the "VariableAndSimpleDataTypes" folder

---
+++ VariablesAndSimpleDataTypes/name_cases.py
@@ -0,0 +1,19 @@
+name = "Eric"
+message = f"Hello {name}, would you like to learn some Python today?"
+
+print(message)
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+famous_quote = "\nAlbert Einstein once said, 'A person who never made a \nmistake never tried anything new'."
+print(famous_quote)
+
+famous_person_name = " Albert Einstein "
+message = f"\n{famous_person_name} once said, 'A person who never made a \nmistake never tried anything new'."
+print(message)
+
+print(famous_person_name.lstrip())
+print(famous_person_name.rstrip())
+print(famous_person_name.strip())
---
+++ numbers.py
@@ -0,0 +1,7 @@
+print(1 + 7)
+print(10 - 2)
+print(2 * 4)
+print(24 / 3)
+
+favorite_number = 10
+print(f"Favorite number is {favorite_number}")
